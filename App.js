56effd825417e58ec58ce07f90a1611b178b2e01
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-05-01 21:55:48 +0530

    basic app is now ready

--- App.js
+++ App.js
@@ -1,42 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  Image,
-  Button,
-  View,
-  Alert,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  let x = 1;
   console.log("App executed");
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        title="Click me"
-        onPress={() =>
-          Alert.alert("Having Fun?", "Want more?", [
-            { text: "Yes", onPress: () => console.log("Yes Pressed") },
-            { text: "No", onPress: () => console.log("No Pressed") },
-          ])
-        }
-      />
-      <Text>Hello World!</Text>
-      <Text>Hello World!</Text>
-      <Image source={require("./assets/favicon.png")} />
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import NativeStatusBarManager from "react-native/Libraries/Components/StatusBar/NativeStatusBarManagerAndroid";
+
+function WelcomeScreen(props) {
+  backgroundImage = require("../assets/background.jpg");
+  logoImage = require("../assets/logo-red.png");
+  return (
+    <ImageBackground style={styles.backgound} source={backgroundImage}>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={logoImage} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgound: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+});
+
+export default WelcomeScreen;
