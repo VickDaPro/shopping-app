3a3de99410236de5b394e64346231c62d1189a5d
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-05-12 14:53:35 +0530

    Added new card component

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   console.log("App executed");
   return <ViewImageScreen />;
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
