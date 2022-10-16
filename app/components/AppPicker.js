a59b79b1213e80a43871921a7ad208f7ec753e24
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-10-17 01:08:09 +0530

    feat: built PickerItem and AppPicker components

---
+++ app/components/AppPicker.js
@@ -0,0 +1,94 @@
+import {
+  Button,
+  FlatList,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import AppText from "./AppText";
+import Screen from "./Screen";
+import PickerItem from "./PickerItem";
+
+const AppPicker = ({
+  icon,
+  placeholder,
+  items,
+  onSelectItem,
+  selectedItem,
+}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.conatiner}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}{" "}
+          </AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => {
+                  setModalVisible(false);
+                  onSelectItem(item);
+                }}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
+  );
+};
+
+export default AppPicker;
+
+const styles = StyleSheet.create({
+  conatiner: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  text: {
+    flex: 1,
+  },
+  textInput: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: "Avenir",
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
