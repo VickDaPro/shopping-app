883f85714078fe2ce818f4c71415ad8c14427fc5
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-10-17 01:05:41 +0530

    feat: built PickerItem and AppPicker components

---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import * as Yup from "yup";
+
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from "../components/forms";
+
+const ListingEditScreen = () => {
+  return (
+    <View>
+      <Text>ListingEditScreen</Text>
+    </View>
+  );
+};
+
+export default ListingEditScreen;
+
+const styles = StyleSheet.create({});
